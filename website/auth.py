97a69997f237011ed2b6f016ef79bf58a8bbd4de
Vineeth <ncvineeth97@gmail.com>
2023-05-20 22:52:55 -0400

    First push

---
+++ website/auth.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
+from . import mysql_obj
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		username = request.form.get('username')
+		password = request.form.get('password')
+		cursor = mysql_obj.connection.cursor()
+		
+		command = 'select * from user_credentials where username = %s and password = MD5(%s)'
+		cursor.execute(command, (username, password))
+
+		if cursor.rowcount == 1:
+			flash("Logged in, I suppose", category='success')
+			session['is_login'] = True
+			session['username'] = username
+			session['page'] = 'home'
+			return redirect(url_for('views.home'))
+		else:
+			flash("Incorrect credentials", category='error')
+
+	return render_template("login.html")
+
+@auth.route('logout')
+def logout():
+	session.pop('is_login', None)
+	session.pop('username', None)
+	session.pop('page', None)
+	return redirect(url_for('auth.login'))
