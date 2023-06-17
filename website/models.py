35a8eb6418e820e1fb707cfecb8ca2d17463c442
Vineeth <ncvineeth97@gmail.com>
2023-06-17 18:01:38 -0400

    Commented code, single record started

--- website/models.py
+++ website/models.py
@@ -4,15 +4,22 @@ from . import mysql_obj
 class User(UserMixin):
     
 	def __init__(self, username, password) -> None:
+		"""
+		The username and the MD5 hash of the password are needed.
+		"""
 		super().__init__()
 		self.username = username
 		self.password = password
 
 	def get(self):
+		"""
+		Checks if the user with the corresponding password hash exists.
+		"""
 		cursor = mysql_obj.connection.cursor()
 		command = 'select * from user_credentials where username = %s and password = MD5(%s)'
 		cursor.execute(command, (self.username, self.password))
 
+		# If there is only one row returned, it means a unique user exists.
 		if cursor.rowcount == 1:
 			return User(self.username, self.password)
 		return None
