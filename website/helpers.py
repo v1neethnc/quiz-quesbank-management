8fff25a226356f9bf608f47a9eac57f31d9a0493
Vineeth <ncvineeth97@gmail.com>
2023-06-17 18:01:38 -0400

    Commented code, single record started

--- website/helpers.py
+++ website/helpers.py
@@ -3,22 +3,29 @@ from datetime import date
 
 class data_inserter():
 	
+	def __init__(self):
+		self.query_obj = mysql_obj
+
+	def command_executer(self, command, data, tp = "select"):
+		cursor = self.sql_obj.connection.cursor()
+		cursor.execute(command, data)
+		if tp == "select":
+			return cursor.fetchall()
+		self.sql_obj.connection.commit()
+		return True
+
 	def insert_record(self, record_type, data_dict):
 		try:
-			cursor = mysql_obj.connection.cursor()
 			if record_type == 'idea':
 				command = 'select max(idea_index) from question_ideas'
-				cursor.execute(command)
-				data = cursor.fetchall()
+				data = self.command_executer(command, (), "select")
 				print(type(data[0][0]))
 				new_index = data[0][0] + 1
 				insert_command = 'insert into question_ideas (idea_index, idea_fact, sources, is_framed) values (%s, %s, %s, "FALSE")'
-				cursor.execute(insert_command, (new_index, data_dict['idea'], data_dict['sources']))
-				mysql_obj.connection.commit()
+				self.command_executer(insert_command, (new_index, data_dict['idea'], data_dict['sources']), "insert")
 			elif record_type == 'note':
 				insert_command = 'insert into notes (notes_text, record_create_date, record_update_date) values (%s, %s, %s)'
-				cursor.execute(insert_command, (data_dict['note'], date.today(), date.today()))
-				mysql_obj.connection.commit()
+				self.command_executer(insert_command, (data_dict['note'], date.today(), date.today()), "insert")
 			return True
 		except:
 			return False
