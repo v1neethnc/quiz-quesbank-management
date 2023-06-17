a51dc626a3fda9e3ebbde0772ba72ff87b658cfa
Vineeth <ncvineeth97@gmail.com>
2023-06-17 18:01:38 -0400

    Commented code, single record started

--- website/fetcher.py
+++ website/fetcher.py
@@ -4,27 +4,38 @@ from configparser import ConfigParser
 class data_fetcher:
     
 	def __init__(self, sql_obj):
+		"""
+		The attributes are as follows:
+		query_obj: contains the MySQL object
+		data_used: contains the data fetched from the last select query
+		config: ConfigParser object that reads the relevant config file
+
+		This class as of 17 June 2023 is primarily used for the display_records page.
+		"""
 		self.query_obj = sql_obj
-		self.is_active = False
 		self.data_used = None
 		self.config = ConfigParser()
 		self.config.read('config.cfg')
 
-	def activate_obj(self):
-		if not self.is_active:
-			self.is_active = True
-
 	def select_query(self):
+		"""
+		Method to identify the page and return the relevant select query
+		"""
+		# Map pages to relevant tables
 		table_map = {'idea': 'question_ideas', 'quiz': 'quizzes', 'note': 'notes'}
-		val = session['page'].split('_')[1]
+		page = session['page'].split('_')[1]
 
-		if val in table_map.keys():
+		# If the page is one of idea, quiz, or note
+		if page in table_map.keys():
+			# Select the right clause that orders data on the page and return the query
 			clause = ''
-			if val == 'quiz':
+			if page == 'quiz':
 				clause = ' order by date_of_quiz asc'
-			elif val == 'note':
+			elif page == 'note':
 				clause = ' order by record_update_date asc'
-			return f"select * from {table_map[val]}{clause}"
+			return f"select * from {table_map[page]}{clause}"
+		
+		# Return the select query for display questions
 		return """select 
 		qd.question_index, qd.question_text, qd.answer_text, qd.answer_explanation, qd.idea_index, qcl.cnm, qd.question_create_date, qd.owner, qd.used_in
 		from questions_data qd inner join question_categories_list qcl 
@@ -32,46 +43,83 @@ class data_fetcher:
 		order by qd.question_index"""
 
 	def data_fetcher(self):
+		"""
+		Runs the select queries and writes data into the data_used attribute
+		"""
 		command = self.select_query()
 		data = self.cursor_execution(command)
 		self.data_used = self.data_preprocessor(data)
 
 	def length_reducer(self, str_list, len_list):
+		"""
+		Given a list of strings and a list of maximum allowed lengths, the method returns a list of 
+		values with the string terminated by an ellipsis, limiting the length to max allowed length
+		"""
 		res_strs = []
 		for string, length in zip(str_list, len_list):
 			res_strs.append(string[:length-3] + "..." if len(string) > length else string)
 		return res_strs
 	
 	def cursor_execution(self, query):
+		"""
+		Creates a cursor from the SQL object, executes the query, and returns the entire dataset fetched
+		"""
 		cursor = self.query_obj.connection.cursor()
 		cursor.execute(query)
 		return cursor.fetchall()
 	
 	def category_fetcher(self):
+		"""
+		Fetches category names from the database, primarily for the filter on display_questions
+		"""
 		command = 'select category_name from categories'
 		return [i[0] for i in self.cursor_execution(command)]
 	
 	def subcategory_fetcher(self):
+		"""
+		Fetches subcategory names from the database, primarily for the filter on display_questions
+		"""
 		command = 'select subcategory_name from subcategories'
 		return [i[0] for i in self.cursor_execution(command)]
 
 	def quizzes_fetcher(self):
+		"""
+		Fetches quizzes indices from the database, primarily for the filter on display_questions
+		"""
 		command = 'select quiz_index from quizzes'
 		return [i[0] for i in self.cursor_execution(command)]
 
 	def authors_fetcher(self):
+		"""
+		Fetches author names from the database, primarily for the filter on display_questions
+		"""
 		command = 'select distinct owner from questions_data'
 		return [self.config['names']['v1'] if auth[0] == self.config['names']['v1'][0] else self.config['names']['v2'] for auth in self.cursor_execution(command)]
 
 	def data_preprocessor(self, data):
-		temp = []
+		"""
+		To perform length reduction and convert datetime objects into proper dates to aid best display
+		standards. The final results are stored in data_to_display. The data stored in data_used is processed
+		for the display pages. For columns put through length_reducer, the full data is also sent in the variable
+		line_to_display so that they may be displayed as a tooltip on the actual webpage.
+		"""
+		data_to_display = []
+		
+		# Display ideas
+		# r_ => column after length_reducer
+		# f_ => actual column data before length_reducer
 		if 'idea' in session['page']:
+			# The order of data is: 
+			# index, r_idea, r_sources, is_framed, f_idea, f_sources
 			for line in data:
 				idea, sources = self.length_reducer([line[1], line[2]], [70, 30])
-				tmp = [line[0], idea, sources, line[3], line[1], line[2]]
-				temp.append(tmp)
-			
+				line_to_display = [line[0], idea, sources, line[3], line[1], line[2]]
+				data_to_display.append(line_to_display)
+		
+		# Display questions
 		if 'question' in session['page']:
+			# The order of data is:
+			# index, r_question, r_answer, r_explanation, idea index, created date, author, r_categories, used in, f_question, f_answer, f_explanation, f_categories
 			for line in data:
 				question, answer, explanation, categories, quizzes = self.length_reducer([line[1], line[2], line[3], line[5].replace(',', ", "), line[8]], [45, 20, 25, 25, 10])
 				date_val = line[6].strftime("%d-%m-%Y")
@@ -79,21 +127,28 @@ class data_fetcher:
 				ques = line[1].replace("\\n", '\n')
 				ans = line[2].replace("\\n", '\n')
 				expl = line[3].replace("\\n", '\n')
-				tmp = [line[0], question, answer, explanation, line[4], date_val, author, categories, quizzes, ques, ans, expl, line[8], line[5].replace(',', ", ")]
-				temp.append(tmp)
+				line_to_display = [line[0], question, answer, explanation, line[4], date_val, author, categories, quizzes, ques, ans, expl, line[8], line[5].replace(',', ", ")]
+				data_to_display.append(line_to_display)
 
+		# Display quizzes
 		if 'quiz' in session['page']:
+			# The order of data is:
+			# quiz_id, event, r_title, quizmasters, venue, date of quiz, number of questions, reception, r_remarks, f_title, f_remarks
 			for line in data:
 				title, reception = self.length_reducer([line[2], line[13]], [30, 25])
 				date_val = line[5].strftime("%d-%m-%Y")
-				tmp = [line[0], line[1], title, line[3], line[4], date_val, line[6], line[12], reception, line[2], line[13]]
-				temp.append(tmp)
-		
+				line_to_display = [line[0], line[1], title, line[3], line[4], date_val, line[6], line[12], reception, line[2], line[13]]
+				data_to_display.append(line_to_display)
+
+		# Display notes
 		if 'note' in session['page']:
+			# The order of data is:
+			# r_note, create date, last updated date, f_note
 			for line in data:
 				note = self.length_reducer([line[1]], [75])
 				create_date = line[2].strftime("%d-%m-%Y")
 				update_date = line[3].strftime("%d-%m-%Y")
-				tmp = [note[0], create_date, update_date, line[1]]
-				temp.append(tmp)
-		return temp
+				line_to_display = [note[0], create_date, update_date, line[1]]
+				data_to_display.append(line_to_display)
+		
+		return data_to_display
--- website/views.py
+++ website/views.py
@@ -4,84 +4,145 @@ from . import helpers
 
 views = Blueprint('views', __name__)
 
-# Decorator
+# Home page on successful login
 @views.route('/home', methods=['GET', 'POST'])
 def home():
-	if 'is_login' in session:
-		return render_template("home.html")
-	return render_template('404.html')
+	# Login updates the session variable
+	if 'is_login' not in session:
+		return render_template('404.html')
+	return render_template("home.html")
 
-@views.route('/redirect-page', methods=['GET'])
+# Temporary route that redirects to a specific set of pages
+@views.route('redirect-page', methods=['GET'])
 def redirect_page():
+	# Data comes through a GET request from an anchor tag on home page
 	page = request.args['page']
+	
+	# Redirect only if the user is logged in
 	if 'is_login' in session:
 		session['page'] = page
+	
+		# New record page
 		if 'new' in page:
 			return redirect(url_for('views.new_record'))
+	
+		# Display record page fetches the data and stores in the data_fetcher object
+		# before redirecting to display records page
 		if 'display' in page:
 			query_obj.data_fetcher()
 			return redirect(url_for('views.display_records'))
+	
 	return render_template('404.html')
 
+# Route for pages to enter a new record into the database
 @views.route("/new-record", methods=['GET', 'POST'])
 def new_record():
+	# Check if the user is logged in
 	if 'is_login' not in session:
 		return render_template('404.html')
+	
+	# After the form on the page is submitted
 	if request.method == 'POST':
 		data_dict = {}
 		print(session['page'])
+
+		# Check what kind of data is being entered into the database
 		if 'idea' in session['page']:
+			# Get the new idea record data and check if the idea field is not null
 			data_dict['idea'] = request.form.get('idea')
 			data_dict['sources'] = request.form.get('sources')
 			if len(data_dict['idea']) != 0:
 				insert_obj = helpers.data_inserter()
+				# Successful or failure message flashing
 				if insert_obj.insert_record('idea', data_dict):
 					flash("Idea inserted into the database.", category="success")
 				else:
 					flash("Idea not inserted into the database.", category="error")
+				# Regardless of success or failure, go to the home page
 				return redirect(url_for('views.home'))
+			# If the idea field is null, then flash an error message and stay on the new record page
 			else:
 				flash("Fill the Idea or Fact form to insert data into the database.", category="error")
 				return render_template('new_record.html')
+		
+		# Entering new note into the database
 		elif 'note' in session['page']:
+			# Get the note text and check if it is null
 			data_dict['note'] = request.form.get('note')
 			if len(data_dict['note']) != 0:
+				# If not null, then run the insert command
 				insert_obj = helpers.data_inserter()
+				# Flash a message based on the result of the insertion
 				if insert_obj.insert_record('note', data_dict):
 					flash("Note inserted into the database.", category="success")
 				else:
 					flash("Note not inserted into the database.", category="error")
 				return redirect(url_for('views.home'))
 			else:
+				# If the note is empty then prompt an error message and stay on the same page
 				flash("Fill the Note form to insert data into the database.", category="error")
 				return render_template('new_record.html')
 			
 	return render_template('new_record.html')
 
+# Route to the display records page
 @views.route("/display-records", methods=['GET', 'POST'])
 def display_records():
+	# Check if the user is logged in
 	if 'is_login' not in session:
 		return render_template('404.html')
 
+	# Fetch data from the query object's attributes
 	data_used = [list(i) for i in query_obj.data_used]
+
+	# Create empty lists that are to be used only when the page is display questions
 	categories, quizzes, authors = [], [], []
 	if 'question' in session['page']:
+		# Update the lists for the display questions page
 		categories = query_obj.category_fetcher()
 		quizzes = query_obj.quizzes_fetcher()
 		authors = query_obj.authors_fetcher()
 		quizzes.append("N/A")
 		authors.append("All")
+
+	# If any of the three buttons are clicked
 	if request.method == 'POST':
+		# Check what kind of page this is and get corresponding button value
+		# TO-DO: Add code for edit and delete buttons 
 		if 'question' in session['page']:
 			q_id = request.form.get('id_val')
 			print(q_id)
-		if 'idea' in session['page']:
+		elif 'idea' in session['page']:
 			q_type = request.form.get('idea_operation')
-			print(q_type)
+			operation, index = q_type.split(' ')[0], int(q_type.split(' ')[1]) - 1
+			print(operation, index, data_used[index])
+			# If the operation is to open the record in a new page
+			if operation == 'more':
+				# Update the session variables
+				session['page'] = 'single_idea'
+				session['data'] = data_used[index]
+				# Redirect to the single record display function
+				return redirect(url_for('views.single_record_display'))
+	# Default display records template
 	return render_template('display_records.html', data=data_used, categories=categories, quizzes=quizzes, authors=authors)
 
-@views.route("/test")
+# Route to the single record display page
+@views.route("/single-record-display")
+def single_record_display():
+	# Check if the user is logged in
+	if 'is_login' not in session:
+		return render_template('404.html')
+	
+	# Fetch the session variables
+	data = session['data']
+
+	# Render the single record display page with the data from the session variable
+	return render_template("single_record_display.html", data=data)
+
+@views.route("/test", methods=['GET'])
 def test_val():
 	if 'is_login' in session:
+		data = request.args['data']
+		print(data)
 		return render_template("test.html")
 	return render_template('404.html')
